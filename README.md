9826a290dc920dfbf0852ec490212d590fee369e
Ivan Ivashnev <ivashnev@phystech.edu>
2019-04-13 22:31:14 +0300

    augmentation works

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Iceberg
-Test task
+Test task: hockey players classification
+# Data cleaning
+The first thing that we should to do is just look at the labeled images that we have. It may be noticed that the images numbered 22, 46, 69, 113, 140, 150, 171 contains players from both teams and must be removed from the training set.
+# Image resizing
+After cleaning we should bring images to uniform size to speed up learning. The large size of all images is 128, so we will resize them to 128x128 size
+# Data augmentation
+The size of training set is very small, so we need to do image augmentation to increase it. 
--- augmentation.py
+++ augmentation.py
@@ -1,5 +1,8 @@
-from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
+from PIL import Image
 import os
+import numpy as np
+from matplotlib import pyplot
 
 
 def image_augmentation(data):
@@ -9,16 +12,48 @@ def image_augmentation(data):
     :param data: name of folder with folders 0 and 1
     """
 
+    # Change directory to folder 'data'
+    parent_dir = os.getcwd()
+    os.chdir(os.path.join(parent_dir, data))
+    folders = os.listdir(os.getcwd())
+
     # Creating image generator
     # We will allow horizontal flip, little rotation and shifts
-    image_datagen = ImageDataGenerator(horizontal_flip=True,
-                                       rotation_range=30,
-                                       width_shift_range=0.2,
-                                       height_shift_range=0.2)
+    image_gen = image.ImageDataGenerator(horizontal_flip=True,
+                                         rotation_range=15,
+                                         width_shift_range=0.1,
+                                         height_shift_range=0.1)
 
-    image_gen = image_datagen.flow_from_directory(data,
-                                                  target_size=(128, 128),
-                                                  batch_size=32,
-                                                  class_mode='binary')
+    train = []
 
-    return image_gen
+    # Start augmenting for each class
+    for folder in folders:
+        # Avoiding hidden folders
+        if not folder.startswith('.'):
+            images = os.listdir(folder)
+            for im in images:
+                try:
+                    img_path = os.path.join(folder, im)
+                    img = Image.open(img_path)
+                    train.append(np.array(img))
+                except Exception as e:
+                    print(e)
+
+            # Converting list into numpy array and correct format
+            train = np.array(train)
+            train = train.reshape(train.shape[0], train.shape[1], train.shape[2], 3)
+
+            # Fitting image generator
+            image_gen.fit(train)
+            os.chdir(os.path.join(parent_dir, data))
+
+            total_image_augmented = 0
+
+            for X_batch in image_gen.flow(train, batch_size=9, save_to_dir=folder, save_prefix='aug',
+                                          save_format='png'):
+                total_image_augmented += 1
+
+            print(total_image_augmented, 'images were augmented for class', folder)
+
+
+image_augmentation('data')
